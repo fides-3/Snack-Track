86abd86755e6d13a75533ab9ed9f8742cdaf014d
ntoiti <fidesluckia36@gmail.com>
2025-06-27 09:20:47 +0300

    returning id and email instead of all fields

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt" as const,
+  },
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || !(await bcrypt.compare(credentials.password, user.password))) {
+          return null;
+        }
+
+        return {
+            id:user?.id,
+            email:user.email,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
