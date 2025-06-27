1db6f9161ecbca3316244062fa2c9fe177fb39d7
ntoiti <fidesluckia36@gmail.com>
2025-06-27 11:00:46 +0300

    commented unnecessary code

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
 
 const prisma = new PrismaClient();
@@ -32,18 +32,18 @@ const user=await prisma.user.create({
   data: { email, password: hashedPassword },
 });
 // ADDING TOKEN IN MY CODE
-const token=jwt.sign({id:user.id,email:user.email}, process.env.JWT_SECRET!,{
-  expiresIn:'7d'
-})
-const response=NextResponse.json({message:"User Created successfully"});
-response.cookies.set('token',token,{
-  httpOnly:true,
-  path:'/',
-  maxAge:60*60*24*7,
-})
+// const token=jwt.sign({id:user.id,email:user.email}, process.env.JWT_SECRET!,{
+//   expiresIn:'7d'
+// })
+return NextResponse.json({message:"User Created successfully"});
+// response.cookies.set('token',token,{
+//   httpOnly:true,
+//   path:'/',
+//   maxAge:60*60*24*7,
+// })
 
 
-    return response
+    // return response
   } catch (error) {
     console.error('API signup error:', error);
     return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
