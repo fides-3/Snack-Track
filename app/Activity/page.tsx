b4d3d6b7bbec8f5f0cf6a401d596a07602c67e16
ntoiti <fidesluckia36@gmail.com>
2025-06-30 14:13:41 +0300

    added useRouter

--- app/Activity/page.tsx
+++ app/Activity/page.tsx
@@ -1,52 +1,86 @@
 "use client"
 import {useState} from 'react'
+import {useRouter} from 'next/navigation'
 function Activity(){
-    const[selectedActivity,setActivity]=useState("")
+    const[activity,setActivity]=useState("")
+    const[message,setMessage]=useState("")
+    
+      const router=useRouter()
+    
+      const handleSubmit= async (e:React.FormEvent)=>{
+        e.preventDefault()
+      
+      if(!activity){
+        setMessage("Please select one of these")
+        return
+      }
+      try{
+        const res=await fetch("/api/me",{
+          method:"PUT",
+          headers:{"Content-Type":"application/json"},
+          body:JSON.stringify({activity})
+        })
+        if(!res.ok){
+          const err=await res.json()
+          setMessage(err.message||"Something went wrong")
+          return
+        }
+        await res.json()
+        setMessage("Activity Saved!")
+        router.push("/Height")
+    
+      }catch(error){
+         console.error("error before submitting activity:" ,error)
+        setMessage("failed to save activity")
+      }
+    }
+    
     return(
         <div className="bg-[url('/backgroundimage.jpg')]  bg-cover bg-center p-8   min-h-screen">
+            <form onSubmit={handleSubmit}>
      <div className=" relative mx-auto p-6 max-w-2xl rounded-full bg-white place-items-center ">
         
                 <p className='font-semibold text-2xl text-center'>🔹How active are you?</p>
                 <div className='flex flex-col justify-center'>
                     <br></br>
                     <label className='flex items-center mb-2'>
-                        <input type="radio" name="sedentary" value="sedentary" checked={selectedActivity==="Sedentary"}
+                        <input type="radio" name="activity" value="Sedentary" checked={activity==="Sedentary"}
                         onChange={(e)=>setActivity(e.target.value)}/>Sedentary
                     </label>
 
                     <label className='flex items-center mb-2'>
-                        <input type="radio" name="lowactive" value="lowactive" checked={selectedActivity==="lowactive"}
+                        <input type="radio" name="activity" value="Low Active" checked={activity==="Low Active"}
                         onChange={(e)=>setActivity(e.target.value)}/>
                         Low Active
                     </label>
 
                      <label className='flex items-center mb-2'>
-                        <input type="radio" name="active" value="active" checked={selectedActivity==="active"}
+                        <input type="radio" name="activity" value="Active" checked={activity==="Active"}
                         onChange={(e)=>setActivity(e.target.value)}/>
                         Active
                     </label>
                     
                     <label className='flex items-center mb-2'>
-                        <input type="radio" name="veryactive" value="veryactive" checked={selectedActivity==="veryactive"}
+                        <input type="radio" name="activity" value="Very Active" checked={activity==="Very Active"}
                         onChange={(e)=>setActivity(e.target.value)}/>
                          Very Active
                     </label>  
                 </div>
                 <div className='flex justify-between items-center mt-8 gap-20'>
-                    <a href='/Gender'>
-                    <button className='flex-row-reverse rounded-lg bg-blue-600  text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 '>Back</button>
                     
-                    </a>
-                    <a href='/Height'>
-                    <button className='flex-row-reverse rounded-lg bg-blue-600  text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 '>Next</button>
+                    <button onClick={()=>router.push("/Gender")} className='flex-row-reverse rounded-lg bg-blue-600  text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 '>Back</button>
                     
-                    </a>
+            
+                    
+                    <button  type="submit" className='flex-row-reverse rounded-lg bg-blue-600  text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 '>Next</button>
+                
                 </div>
                 
                 
                 
 
             </div>
+            </form>
         </div>
     )
 
