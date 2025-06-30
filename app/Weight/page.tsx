9ee8fcfde796c5ea5ae674cc303514def15d478e
ntoiti <fidesluckia36@gmail.com>
2025-06-30 12:02:45 +0300

    added backend functionalities

--- app/Weight/page.tsx
+++ app/Weight/page.tsx
@@ -1,15 +1,55 @@
 "use client"
-import { useState } from "react";
+import { useState} from "react";
+import {useRouter } from "next/navigation"
 
 export default function Weight() {
   const [unit, setUnit] = useState("kg");
   const [showUnits, setShowUnits] = useState(false);
+  const [message,setMessage]=useState("")
+  const [weight,setWeight]=useState("")
   const units = ["kg", "lb" , "pounds"];
   <br></br>
+
+  const router=useRouter()
+
+
+ const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+   
+
+    if(!weight){
+      setMessage("Please enter your weight");
+      return;
+    }
+     console.log("submitting weight",weight)
+
+     try{
+    const res = await fetch("/api/me", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ weight:parseFloat(weight) }), 
+    });
+
+    if (!res.ok) {
+      const err=await res.json()
+      setMessage(err.message||"Something went wrong");
+      return
+    }
+    await res.json()
+    setMessage("weight saved!")
+    router.push("/Age")
+  }catch(error){
+    console.error("error before submitting weight:" ,error)
+    setMessage("failed to save weight")
+  }
+  };
+
+
 
   return (
     <div className="min-h-screen bg-[url('/backgroundimage.jpg')] bg-cover bg-center p-8 ">
       <br></br>
+      <form onSubmit={handleSubmit}>
     <div className=" relative rounded-full bg-white place-items-center mx-auto p-6 max-w-2xl  ">
       <p className="font-semibold text-2xl "> 🔹What is your weight?</p>
     
@@ -18,7 +58,9 @@ export default function Weight() {
       <input
         type="number"
         placeholder=" "
+        value={weight}
         className="w-full outline-none  bg-transparent text-lg"
+        onChange={(e)=>setWeight(e.target.value)}
       />
 
       {/* Unit Display */}
@@ -62,18 +104,19 @@ export default function Weight() {
     </div>
     <br></br>
     <div className="flex justify-between mt-6 items-center  gap-20">
-      <a href="/Height ">
-        <button className=" bg-blue-600  rounded-lg flex-row-reverse  text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300  ">Back</button>
-      </a>
-      <a href="/Age">
-        <button className="bg-blue-600   text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 rounded-lg flex-row-reverse  ">
+     
+        <button onClick={()=>router.push("/Height")}className=" bg-blue-600  rounded-lg flex-row-reverse  text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300  ">Back</button>
+    
+     
+        <button className="bg-blue-600   text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 rounded-lg flex-row-reverse  " type="submit">
           Next
         </button>
-      </a>
+    
     </div>
 
 
     </div>
+    </form>
     </div>
   );
 }
