61ec67652d278093f614f54929a1d087eeaacb95
ntoiti <fidesluckia36@gmail.com>
2025-06-30 14:15:06 +0300

    added async functions

--- app/Height/page.tsx
+++ app/Height/page.tsx
@@ -1,15 +1,48 @@
 "use client"
+import {useRouter} from 'next/navigation'
 import { useState } from "react";
 
 export default function Height() {
   const [unit, setUnit] = useState("M");
+  const[height,setHeight]=useState("")
   const [showUnits, setShowUnits] = useState(false);
   const units = ["M", "Feet" , "Cm"];
+  const[message,setMessage]=useState("")
+         const router=useRouter()
+              
+                const handleSubmit= async (e:React.FormEvent)=>{
+                  e.preventDefault()
+                
+                if(!height){
+                  setMessage("Please select one of these options")
+                  return
+                }
+                try{
+                  const res=await fetch("/api/me",{
+                    method:"PUT",
+                    headers:{"Content-Type":"application/json"},
+                    body:JSON.stringify({height:parseFloat(height)})
+                  })
+                  if(!res.ok){
+                    const err=await res.json()
+                    setMessage(err.message||"Something went wrong")
+                    return
+                  }
+                  await res.json()
+                  setMessage("Option Saved!")
+                  router.push("/Weight")
+              
+                }catch(error){
+                   console.error("error before submitting option:" ,error)
+                  setMessage("failed to save option")
+                }
+              }
   <br></br>
 
   return (
     <div className="min-h-screen bg-[url('/backgroundimage.jpg')] bg-cover bg-center p-8 ">
       <br></br>
+      <form onSubmit={handleSubmit}>
     <div className=" relative rounded-full bg-white place-items-center mx-auto p-6 max-w-2xl  ">
       <p className="font-semibold text-2xl "> 🔹How tall are you?</p>
     
@@ -18,6 +51,8 @@ export default function Height() {
       <input
         type="number"
         placeholder=" "
+        value={height}
+        onChange={(e)=>setHeight(e.target.value)}
         className="w-full outline-none bg-transparent text-lg"
       />
 
@@ -62,18 +97,21 @@ export default function Height() {
     </div>
     <br></br>
     <div className="flex justify-between items-center mt-8 gap-20">
-      <a href="/Activity ">
-        <button className=" bg-blue-600  rounded-lg flex-row-reverse  text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300   ">Back</button>
-      </a>
-      <a href="/Weight">
+      
+        <button onClick={()=>router.push("/Activity")} className=" bg-blue-600  rounded-lg flex-row-reverse  text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300   ">Back</button>
+      
+      
         <button className="bg-blue-600   text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 rounded-lg flex-row-reverse  ">
           Next
         </button>
-      </a>
+      
     </div>
 
 
     </div>
+    </form>
     </div>
   );
 }
+
+
