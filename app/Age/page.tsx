435f4759361a8934814ac72239fb6201ee4906e0
ntoiti <fidesluckia36@gmail.com>
2025-06-30 14:14:04 +0300

    integrated onSubmit

--- app/Age/page.tsx
+++ app/Age/page.tsx
@@ -1,12 +1,46 @@
-
+"use client"
+import {useState} from 'react'
+import {useRouter} from 'next/navigation'
 
 export default function Age() {
- 
-  <br></br>
+  const[age,setAge]=useState("")
+  const[message,setMessage]=useState("")
+
+  const router=useRouter()
+
+  const handleSubmit= async (e:React.FormEvent)=>{
+    e.preventDefault()
+  
+  if(!age){
+    setMessage("Please enter your age")
+    return
+  }
+  try{
+    const res=await fetch("/api/me",{
+      method:"PUT",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify({age:parseInt(age)})
+    })
+    if(!res.ok){
+      const err=await res.json()
+      setMessage(err.message||"Something went wrong")
+      return
+    }
+    await res.json()
+    setMessage("Age Saved!")
+    router.push("/userinfo")
+
+  }catch(error){
+     console.error("error before submitting weight:" ,error)
+    setMessage("failed to save weight")
+  }
+}
+
 
   return (
     <div className="min-h-screen bg-[url('/backgroundimage.jpg')] bg-cover bg-center p-8 ">
       <br></br>
+      <form onSubmit={handleSubmit}>
     <div className=" relative rounded-full bg-white place-items-center mx-auto p-6 max-w-2xl  ">
       <p className="font-semibold text-2xl "> 🔹What is your age?</p>
     
@@ -15,6 +49,8 @@ export default function Age() {
       <input
         type="number"
         placeholder=" "
+        value={age}
+        onChange={(e)=>setAge(e.target.value)}
         className="w-full outline-none bg-transparent text-lg"
       />
        <span className="ml-2">Years</span>
@@ -24,16 +60,17 @@ export default function Age() {
     
     <br></br>
     <div className="flex justify-between mt-8 items-center gap-20">
-      <a href="/Height ">
-        <button className=" bg-blue-600 rounded-lg flex-row-reverse  text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 ">Back</button>
-      </a>
-      <a href="/Age">
-        <button className="bg-blue-600   text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 rounded-lg flex-row-reverse  ">
+      
+        <button onClick={()=>router.push("/weight")} className=" bg-blue-600 rounded-lg flex-row-reverse  text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 ">Back</button>
+    
+    
+        <button type="submit" className="bg-blue-600   text-white hover:bg-blue-700 font-semibold px-6 py-2 shadow-md transition duration-300 rounded-lg flex-row-reverse  ">
           Next
         </button>
-      </a>
+    
     </div>
     </div>
+    </form>
     </div>
 
 
