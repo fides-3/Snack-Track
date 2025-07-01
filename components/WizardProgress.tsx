06f08296a34c6e9bc3bb39924cbaede9f9bdd549
ntoiti <fidesluckia36@gmail.com>
2025-07-01 11:57:13 +0300

    removed some lines of code

--- components/WizardProgress.tsx
+++ components/WizardProgress.tsx
@@ -1,17 +1,17 @@
 type WizardProgressProps = {
   currentStep: number;
   totalSteps: number;
-  stepLabels?: string[];
+
 };
 
-export default function WizardProgress({ currentStep, totalSteps, stepLabels }: WizardProgressProps) {
+export default function WizardProgress({ currentStep, totalSteps }: WizardProgressProps) {
   const progressPercent = (currentStep / totalSteps) * 100;
 
   return (
     <div className="w-40 items-center justify-center  mb-6 px-4">
       <div className="flex  text-sm text-black mb-1">
         <p>Step {currentStep} of {totalSteps}</p>
-        {stepLabels && <p>{stepLabels[currentStep - 1]}</p>}
+  
       </div>
 
       <div className="w-40 bg-gray-100 h-2 rounded-full">
