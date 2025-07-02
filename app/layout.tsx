eb835f54324e9b5df35476271e6d2e6dbd0b8ffc
ntoiti <fidesluckia36@gmail.com>
2025-07-02 14:39:26 +0300

    removed some code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import "./globals.css";
 import MainLayout from "@/components/MainLayout"
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import {Providers} from "./providers"
 
-export const metadata: Metadata = {
-  title: "Snack & Track",
-  description: "Track your nutrition with ease",
-};
+// export const metadata: Metadata = {
+//   title: "Snack & Track",
+//   description: "Track your nutrition with ease",
+// };
 
 export default function RootLayout({
   children,
