95c5ff5f419d65c6c75eea105168c28449ead2d8
ntoiti <fidesluckia36@gmail.com>
2025-07-08 13:48:57 +0300

    imported font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,16 @@ import "./globals.css";
 import MainLayout from "@/components/MainLayout"
 // import type { Metadata } from "next";
 import {Providers} from "./providers"
+import {Geist} from 'next/font/google'
 
 // export const metadata: Metadata = {
 //   title: "Snack & Track",
 //   description: "Track your nutrition with ease",
 // };
+const GeistFont=Geist({
+  subsets:['latin'],
+  display:"swap"
+})
 
 export default function RootLayout({
   children,
@@ -14,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={GeistFont.className}>
       <body>
 
         <Providers>
