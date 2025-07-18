b35728c3a5db95cb9dac68599bd78c24d08326c7
ntoiti <fidesluckia36@gmail.com>
2025-07-18 12:41:55 +0300

    added cod

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,134 @@
+
+
+
+'use client';
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Mail, Eye, EyeOff } from "lucide-react";
+
+
+export default function forgotPassword() {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  
+
+  const [message, setMessage] = useState('');
+
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setMessage('');
+
+    try{
+    const res = await fetch('api/forgot-password',{
+    method:'POST',
+    headers:{'Content-type':'application/json'},
+    body:JSON.stringify({email})
+
+    })
+    const data=await res.json()
+    if(res.ok){
+      setMessage("Check your email for the password reset link")
+    }
+    else{
+      setMessage(data.message || "Something went wrong")
+    }
+  }catch(err){
+    console.error("Forgot Password Error",err);
+    setMessage("Failed to send email")
+
+  }
+}
+
+
+  return (
+    <div className=" bg-[url('/backgroundimage.jpg')] bg-cover bg-center  min-h-screen">
+      <div className=" flex justify-center  min-h-screen  items-center ">
+      <form onSubmit={handleSubmit} className="bg-white p-8 w-full rounded-2xl shadow-2xl max-w-md">
+        
+      <div className="text-center mb-6">
+      <h1 className=" font-semibold">FORGOT PASSWORD</h1>
+      </div>
+      <div className=" space-y-4">
+        
+         <div className="relative">
+       
+          <input
+            id="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border px-4 py-2 w-full rounded-md text-black  focus:outline-none focus:ring-2 focus:ring-blue-400"
+            required
+          />
+          
+        </div>
+         
+        <button className="text-white w-full rounded-md transition py-2 hover:bg-blue-500 bg-blue-400" type="submit">
+          SEND
+        </button>
+       
+        <div className="text-center text-sm">
+        <p>Return to {""}
+          <Link href="/" className="text-blue-400">
+           login
+           </Link>
+           </p>
+           </div>
+        
+
+         
+        {message && <p className="text-red-500 mt-2 text-center">{message}</p>}
+        
+      
+         </div>
+      </form>
+      </div>
+    </div>
+)}
+    
+//   <div className="flex items-center justify-center min-h-screen bg-white">
+//     <form onSubmit={handleSubmit} className="bg-white p-8 rounded-2xl shadow-2xl w-full max-w-md">
+//       <div className="text-center mb-6">
+//         <h1 className="text-2xl font-semibold">FORGOT PASSWORD</h1>
+//       </div>
+
+//       <div className="space-y-4">
+//         <div className="relative">
+//           <input
+//             id="email"
+//             type="email"
+//             placeholder="Email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             className="border px-4 py-2 w-full rounded-md text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
+//             required
+//           />
+//         </div>
+
+//         <button
+//           className="bg-blue-500 hover:bg-blue-600 text-white w-full py-2 rounded-md transition"
+//           type="submit"
+//         >
+//           SEND
+//         </button>
+
+//         <div className="text-center text-sm">
+//           <p>
+//             Return to{" "}
+//             <Link href="/" className="text-blue-500 underline">
+//               login
+//             </Link>
+//           </p>
+//         </div>
+
+//         {message && <p className="text-red-500 text-center">{message}</p>}
+//       </div>
+//     </form>
+//   </div>
+// );
+
+  
