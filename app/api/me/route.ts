5db22e15d2d33218e7ec03e5e45db3234cd58ce1
ntoiti <fidesluckia36@gmail.com>
2025-07-22 10:10:53 +0300

    changed path

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -1,7 +1,7 @@
 
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/authOptions';
 import { prisma } from '@/lib/prisma'; 
 //READS DATA ENTERED BY USER IN THE FORMS
 export async function GET(req: NextRequest) {
