c90f3e46c348f5531508aa41a56dc4d187ad7ff9
ntoiti <fidesluckia36@gmail.com>
2025-07-31 16:11:39 +0300

    added code

--- app/Activity/page.tsx
+++ app/Activity/page.tsx
@@ -37,16 +37,16 @@ function Activity(){
     }
     
     return(
-        <div className="bg-[url('/backgroundimage.jpg')]  bg-cover bg-center p-8   min-h-screen">
+        <div className="bg-[url('/backgroundimage.jpg')]  dark:bg-[url('/darkbg.jpg')] bg-cover bg-center p-8   min-h-screen">
             <form onSubmit={handleSubmit}>
-     <div className=" relative mx-auto p-6 max-w-2xl rounded-full bg-white place-items-center ">
+     <div className=" relative mx-auto p-6 max-w-2xl rounded-full bg-white dark:bg-black place-items-center ">
       <WizardProgress currentStep={3} totalSteps={6}  />
      
         
-                <p className='font-semibold text-2xl text-center'>🔹How active are you?</p>
+                <p className='font-semibold text-2xl text-center text-black dark:text-white'>🔹How active are you?</p>
                 <div className='flex flex-col justify-center'>
                     <br></br>
-                    <label className='flex items-center mb-2'>
+                    <label className='flex items-center text-black dark:text-white mb-2'>
                         <input type="radio" name="activity" value="Sedentary" checked={activity==="Sedentary"}
                         onChange={(e)=>setActivity(e.target.value)}/>Sedentary
                     </label>
