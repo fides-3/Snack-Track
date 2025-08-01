3a8021047ba6f9111239864aa07e138aaac1e914
ntoiti <fidesluckia36@gmail.com>
2025-08-01 11:46:07 +0300

    added icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,25 +1,72 @@
 "use client"
 import Link from "next/link";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdCreate } from "react-icons/md";
+import { LuHandshake } from "react-icons/lu";
+import {GoGoal} from 'react-icons/go'
+import {BiMaleFemale} from 'react-icons/bi'
+import  {FiActivity} from 'react-icons/fi'
+import {GiBodyHeight} from 'react-icons/gi'
+import { FaWeight } from "react-icons/fa";
+import { FaRegHourglass } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { CiFaceSmile } from "react-icons/ci";
 function Sidebar(){
     return(
-        <div className="w-48 fixed left-0 top-0   bg-white dark:bg-black dark:text-white text-black space-y-4 h-screen">
-            <img src="/welcome1.jpg " className="pt-4 pb-0 pl-4  " width={450}/>
+        <div className="w-36 fixed  left-0 top-16  bg-white dark:bg-black  dark:text-white  text-black  h-screen">
 
-            <ul className="flex flex-col pl-12 pt-0 font-semibold space-y-3">
-                <Link href="/" className="hover:underline dark:text-white">Home</Link>
-                <Link href="/signup" className="hover:underline">Signup</Link>
-                <Link href="/welcome" className="hover:underline">Welcome</Link>
-                <Link href="/Goal" className="hover:underline">Goals</Link>
-                <Link href="/Gender" className="hover:underline">Gender</Link>
-                <Link href="/Activity" className="hover:underline">Activity</Link>
-                <Link href="/Height" className="hover:underline">Height</Link>
-                <Link href="/Weight" className="hover:underline">Weight</Link>
-                <Link href="/Age" className="hover:underline">Age</Link>
-                <Link href="/account" className="hover:underline">Profile</Link>
-                <Link href="/userinfo" className="hover:underline">Me</Link>
+            {/* <img src="/welcome1.jpg " className="pt-4 pb-0 pl-4  " width={450}/> */}
+            
+            <ul className="flex flex-col pl-12 pt-0 font-md  space-y-5">
+                <div className="flex  items-center border-b border-gray-500 ">
+                    <IoHomeOutline className="space-x-2 w-5 h-5  dark:text-white"></IoHomeOutline>
+                <Link href="/" className=" dark:text-white">Home</Link>
+                </div>
+                <div className="flex items-center w-full border-b border-gray-500 ">
+                    <MdCreate className="space-x-2 w-5 h-5  dark:text-white"></MdCreate>
+                <Link href="/signup">Signup</Link>
+                </div>
+                <div className="flex items-center border-b border-gray-500 ">
+                    <LuHandshake className="space-x-2 w-5 h-5"></LuHandshake>
+                <Link href="/welcome">Welcome</Link>
+                </div>
+                <div className="flex border-b border-gray-500  items-center">
+                    <GoGoal className="space-x-4 w-5 h-5"></GoGoal>
+                <Link href="/Goal" >Goals</Link>
+                </div>
+                <div className="flex items-center border-b border-gray-500 ">
+                    <BiMaleFemale className="space-x-2 w-5 h-5"></BiMaleFemale>
+                <Link href="/Gender" >Gender</Link>
+                </div>
+                <div className="flex items-center border-b border-gray-500 ">
+                    <FiActivity className="space-x-2 w-5 h-5"></FiActivity>
+                <Link href="/Activity" >Activity</Link>
+                </div>
+                <div className="flex items-center border-b border-gray-500 ">
+                    <GiBodyHeight className="space-x-2 w-5 h-5"></GiBodyHeight>
+                <Link href="/Height" >Height</Link>
+                </div>
+                <div className="flex items-center border-b border-gray-500 ">
+                    <FaWeight className="space-x-2 w-5 h-5"></FaWeight>
+                <Link href="/Weight">Weight</Link>
+                </div>
+
+                <div className="flex items-center border-b border-gray-500 ">
+                    <FaRegHourglass className="w-5 h-5 space-x-2"></FaRegHourglass>
+                <Link href="/Age">Age</Link>
+                </div>
+
+                <div className="flex items-center border-b border-gray-500 ">
+                    <CgProfile className="w-5 h-5 space-x-2"></CgProfile>
+                <Link href="/account" >Profile</Link>
+                </div>
+                <div className="flex items-center border-b border-gray-500 ">
+                    <CiFaceSmile className="w-5 h-5 space-x-2"></CiFaceSmile>
+                <Link href="/userinfo">Me</Link>
+                </div>
             </ul>
-
-        </div>
+           </div>
+        
     )
 
 }
