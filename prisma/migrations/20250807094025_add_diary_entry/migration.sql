46770bb3c9726b5f9c4f2ea11093b46a8d466b2b
ntoiti <fidesluckia36@gmail.com>
2025-08-07 21:33:55 +0300

    did some migrations

---
+++ prisma/migrations/20250807094025_add_diary_entry/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `DiaryEntry` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `food` VARCHAR(191) NOT NULL,
+    `calories` DOUBLE NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `DiaryEntry` ADD CONSTRAINT `DiaryEntry_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
