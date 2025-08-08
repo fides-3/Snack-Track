a6f973f3abb1304b190bf48d76e975eb47966079
ntoiti <fidesluckia36@gmail.com>
2025-08-08 16:03:32 +0300

    added code

--- components/CalendarPicker.tsx
+++ components/CalendarPicker.tsx
@@ -7,25 +7,65 @@ import "react-datepicker/dist/react-datepicker.css"
 export default function CalendarPicker(){
     const[startDate,setStartDate]=useState<Date|null>(new Date())
     const[showCalendar,setShowCalendar]=useState<boolean>(false)
+    const[entries,setEntries]=useState<any[]>([])
+    const[showEntries,setShowEntries]=useState<boolean>(false)
+
+    const fetchEntriesfordate=async(date:Date)=>{
+        const formattedDate=date.toISOString().split('T')[0]
+        const res=await fetch(`api/diary?date=${formattedDate}`)
+        const data=await res.json()
+        setEntries(data)
+        setShowEntries(true)
+    }
 
     return(
         <div className='relative inline-block text-left'>
-            <button onClick={()=>setShowCalendar((prev:boolean)=>!prev)}
+            <button onClick={()=>{
+                 setShowCalendar((prev:boolean)=>!prev)
+                 setShowEntries(false)
+            }}
                 className='p-2 rounded-full hover:bg-slate-600'>
                 <LuCalendarDays className='w-6 h-6'/>
             </button>
 
             {showCalendar&&(
-                <div className='absolute right-0 z-20 mt-2'>
+                <div className='absolute right-0 top-0  mt-2'>
                     <DatePicker
                     selected={startDate}
                     onChange={(date)=>{
                         setStartDate(date!)
                         setShowCalendar(false)
+                        fetchEntriesfordate(date!)
+                       
                     }}
                     inline
                     />
                 </div>
+            )}
+           
+
+            {showEntries&&startDate &&(
+                <div className=' bg-white p-2 shadow rounded '>
+                    <h2 className='font-semibold'>
+                        Entries for {startDate.toDateString()}
+                    </h2>
+                    {entries.length>0?(
+                        <ul className='list-disc list-inside '>
+                            {entries.map((entry)=>(
+                                <li key={entry.id}>
+                                    {entry.food}-{entry.calories} kcal
+                                </li>
+
+                            ))}
+
+                        </ul>
+                    ):(
+                        <p className='text-blue-500'>No entries for this date</p>
+                        
+                    
+                    )}
+                </div>
+
             )}
             
         </div>
