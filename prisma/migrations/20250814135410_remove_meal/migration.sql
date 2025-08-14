78f2c9ba75949d320952ac602363ac97066eb027
ntoiti <fidesluckia36@gmail.com>
2025-08-14 20:36:56 +0300

    did migrations

---
+++ prisma/migrations/20250814135410_remove_meal/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `meal` on the `diary` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `diary` DROP COLUMN `meal`;
