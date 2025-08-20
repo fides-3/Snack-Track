d9dfd275fbc27cc3614dacefd66d9933177df148
ntoiti <fidesluckia36@gmail.com>
2025-08-20 15:18:44 +0300

    added more styling

---
+++ components/header.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Header(){
+
+
+
+    return(
+        <header className="bg-blue-700 shadow-md  w-full rounded-t-md p-2 ">
+            <div className="p-4 flex items-center justify-center">
+                <h1 className="font-semibold text-md">🤖CHAT WITH OUR AI ASSISTANT</h1>
+            </div>
+        </header>
+    )
+}
